7697ca71e0ccf43cc14f026eb14ea43cce7867cf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-21 17:57:19 -0300

    Projecto kafka desenvolvimento

--- sb-kafka-producer/src/main/java/com/masterdev/sb_kafka_producer/producer/PagamentoProducer.java
+++ sb-kafka-producer/src/main/java/com/masterdev/sb_kafka_producer/producer/PagamentoProducer.java
@@ -1,5 +1,24 @@
 package com.masterdev.sb_kafka_producer.producer;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
 public class PagamentoProducer {
 
+	@Value("${topicos.pagamentos.request.topic}")
+	private String topic;
+	
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	
+	private final ObjectMapper objectMapper;
+	
+	
+	public PagamentoProducer(KafkaTemplate<String, String> kafkaTemplate,ObjectMapper objectMapper) {
+		this.kafkaTemplate=kafkaTemplate;
+		this.objectMapper=objectMapper;
+	}
 }
